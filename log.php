e6b950ec5c5ea3289ee20a31333dfa690a7f00b7
kurabe <arkangel.exe@gmail.com>
2015-09-07 17:55:23 -0500

    Cambio solo por prueba

--- log.php
+++ log.php
@@ -6,7 +6,6 @@
     
     
     
-    
         <style>
       /* NOTE: The styles were added inline because Prefixfree needs access to your styles and they must be inlined if they are on local disk! */
       @import url(http://fonts.googleapis.com/css?family=Exo:100,200,400);
@@ -174,7 +173,7 @@ p{
 		</div>
 		<br>
         <form action="System/php/procesar_login.php" method="POST" class="login">
-				<input type="text" placeholder="tucorreo@algo.com" name="nombre"><br>
+				<input type="text" placeholder="user@host.com" name="nombre"><br>
 				<input type="password" placeholder="password" name="password"><br>
 				<input type="submit" value="Login">
       	</form>
