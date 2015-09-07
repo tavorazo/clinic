0cac3f0b6e213d14e883086a329b40e852b0c566
OctavioRazo <octavio.razo.verduzco@gmail.com>
2015-09-07 11:28:41 -0500

    DB connection

--- System/php/base2.php
+++ System/php/base2.php
@@ -1,3 +1,4 @@
 <?php
-	include("http://www.endoperio.com.mx/System/php/base2.php");
+	$dbh = mysql_connect('endoperio.com.mx:3306','endoper2_git','T~J{QM=4F;Q#') or die('Error de conexion: ' . mysql_error() );
+	$base = mysql_select_db('endoper2_endoperio', $dbh) or die('Error de seleccion de base: ' . mysql_error() );
 ?>
