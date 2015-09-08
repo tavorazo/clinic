127ab206ae543a1389df31184e8d7db039a67acc
OctavioRazo <octavio.razo.verduzco@gmail.com>
2015-09-07 21:03:46 -0500

    modifiqued bases

--- System/php/base.php
+++ System/php/base.php
@@ -1,4 +1,14 @@
 <?php
-	$dbh = mysql_connect('endoperio.com.mx:3306','endoper2_git','T~J{QM=4F;Q#') or die('Error de conexion: ' . mysql_error() );
-	$base = mysql_select_db('endoper2_endoperio', $dbh) or die('Error de seleccion de base: ' . mysql_error() );
+	//$dbh = mysql_connect('endoperio.com.mx:3306','endoper2_git','T~J{QM=4F;Q#') or die('Error de conexion: ' . mysql_error() );
+	//$base = mysql_select_db('endoper2_1', $dbh) or die('Error de seleccion de base: ' . mysql_error() );
+
+//$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$server = "endoperio.com.mx:3306";
+$username = "endoper2_git";
+$password = "T~J{QM=4F;Q#";
+$db = "endoper2_1";
+
+$conn = new mysqli($server, $username, $password, $db);
+
 ?>
--- System/php/procesar_login.php
+++ System/php/procesar_login.php
@@ -3,18 +3,19 @@
 
 <body>
 	<?php
+	echo "entroaqui";
 	@session_start();
 	$a=$_POST['nombre'];
 	$b=$_POST['password'];
-	//echo $a;
-	//echo $b;
-	include("http://www.endoperio.com.mx/System/php/base.php");
-	//include("base.php");
-	$select = 'select * from usuarios where correo="'.$a.'" and password="'.$b.'";';
-	//echo $select, "<br>";
-	$resul = mysql_query($select, $dbh) or die ("problema con la solicitud");
-	$renglon = mysql_fetch_assoc($resul);
+
+	require_once("base.php");
+
+	$select = 'SELECT * from usuarios where correo="'.$a.'" and password="'.$b.'";';
+
+	$resul = mysqli_query($select, $conn);
+	$renglon = mysqli_fetch_assoc($resul);
 	
+	echo $resul;	
 	
 	echo "Verificando. . .";
   	?>
@@ -23,7 +24,7 @@
 	<?php
 	if($renglon['correo']==''){
 		
-		echo '<br><h2>Tu inicio de sesi&oacute;n ha sido incorrecto <br> vuelve a intentar  <META HTTP-EQUIV="Refresh" CONTENT="1; URL=http://www.endoperio.com.mx/log.php">';
+		echo '<br><h2>Tu inicio de sesi&oacute;n ha sido incorrecto <br> vuelve a intentar  <META HTTP-EQUIV="Refresh" CONTENT="1; URL=../../log.php">';
 	}
 	else{
 		$_SESSION['u']=$renglon['id_usuario'];
@@ -31,9 +32,10 @@
 		$_SESSION['rol']=$renglon['rol'];
 		$_SESSION['nombres'] = $renglon['nombres'];
 		echo "<br>Correcto. . .";
-		echo '<META HTTP-EQUIV="Refresh" CONTENT="0; URL=http://www.endoperio.com.mx/System/panel.php">';
+		echo '<META HTTP-EQUIV="Refresh" CONTENT="0; URL=/System/panel.php">';
 	}
+	mysqli_close ($conn);
+
 	?>
-	
 	</body>
 </html>
