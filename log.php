6b3b421b2c87f5939df5b77bdf4bd231b038b508
Octavio Razo <octavio.razo.verduzco@gmail.com>
2015-09-09 12:19:39 -0500

    lo

--- log.php
+++ log.php
@@ -4,42 +4,180 @@
     <meta charset="UTF-8">
     <title>Random Login Form</title>
     
-<style>
-@import url(http://fonts.googleapis.com/css?family=Exo:100,200,400);
+    
+    
+    
+        <style>
+      /* NOTE: The styles were added inline because Prefixfree needs access to your styles and they must be inlined if they are on local disk! */
+      @import url(http://fonts.googleapis.com/css?family=Exo:100,200,400);
 @import url(http://fonts.googleapis.com/css?family=Source+Sans+Pro:700,400,300);
-body{ margin: 0; padding: 0; background: #fff; color: #fff; font-family: Arial; font-size: 12px; }
-.body{ position: absolute; top: -20px; left: -20px; right: -40px; bottom: -40px; width: auto; height: auto; background-image: url(http://www.dentaluna.com/wp-content/uploads/2015/01/di%C5%9F-beyazlatma-i%C5%9F.jpg); background-size: cover; -webkit-filter: blur(5px); z-index: -1; }
-.grad{ position: absolute; top: -20px; left: -20px; right: -40px; bottom: -40px; width: auto; height: auto; background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,rgba(0,0,0,0)), color-stop(100%,rgba(0,0,0,0.65))); /* Chrome,Safari4+ */ z-index: 1; opacity: 0.7; }
-.header{ position: absolute; top: calc(50% - 35px); left: calc(50% - 255px); z-index: 2; }
-p{ z-index: 1000; text-align: center; color: #fff; font-family: 'Exo', sans-serif; font-size: 35px; font-weight: 200; }
-.header div{ float: left; color: #fff; font-family: 'Exo', sans-serif; font-size: 35px; font-weight: 200; }
-.header div span, p span{ color: #5379fa !important }
-.login{ position: absolute; top: calc(50% - 75px); left: calc(50% - 50px); height: 150px; width: 350px; padding: 10px; z-index: 2; }
-.login input[type=text]{ width: 250px; height: 30px; background: transparent; border: 1px solid rgba(255,255,255,0.6); border-radius: 2px; color: #fff; font-family: 'Exo', sans-serif; font-size: 16px; font-weight: 400; padding: 4px; } 
-.login input[type=submit]{ width: 260px; height: 35px; background: #fff; border: 1px solid #fff; cursor: pointer; border-radius: 2px; color: #a18d6c; font-family: 'Exo', sans-serif; font-size: 16px; font-weight: 400; padding: 6px; margin-top: 10px }
-.login input[type=button]:hover{ opacity: 0.8; }
-.login input[type=submit]:active{ opacity: 0.6; }
-.login input[type=text]:focus{ outline: none; border: 1px solid rgba(255,255,255,0.9); }
-.login input[type=password]:focus{ outline: none; border: 1px solid rgba(255,255,255,0.9); }
-.login input[type=submit]:focus{ outline: none; }
-::-webkit-input-placeholder{  color: rgba(255,255,255,0.6); }
-::-moz-input-placeholder{  color: rgba(255,255,255,0.6); }
-</style>
 
+body{
+  margin: 0;
+  padding: 0;
+  background: #fff;
+
+  color: #fff;
+  font-family: Arial;
+  font-size: 12px;
+}
+
+.body{
+  position: absolute;
+  top: -20px;
+  left: -20px;
+  right: -40px;
+  bottom: -40px;
+  width: auto;
+  height: auto;
+  background-image: url(http://www.dentaluna.com/wp-content/uploads/2015/01/di%C5%9F-beyazlatma-i%C5%9F.jpg);
+  background-size: cover;
+  -webkit-filter: blur(5px);
+  z-index: -1;
+}
+
+.grad{
+  position: absolute;
+  top: -20px;
+  left: -20px;
+  right: -40px;
+  bottom: -40px;
+  width: auto;
+  height: auto;
+  background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,rgba(0,0,0,0)), color-stop(100%,rgba(0,0,0,0.65))); /* Chrome,Safari4+ */
+  z-index: 1;
+  opacity: 0.7;
+}
+
+.header{
+  position: absolute;
+  top: calc(50% - 35px);
+  left: calc(50% - 255px);
+  z-index: 2;
+}
+p{
+  z-index: 1000;
+  text-align: center;
+  color: #fff;
+  font-family: 'Exo', sans-serif;
+  font-size: 35px;
+  font-weight: 200;
+}
+
+.header div{
+  float: left;
+  color: #fff;
+  font-family: 'Exo', sans-serif;
+  font-size: 35px;
+  font-weight: 200;
+}
+
+.header div span, p span{
+  color: #5379fa !important;
+}
+
+.login{
+  position: absolute;
+  top: calc(50% - 75px);
+  left: calc(50% - 50px);
+  height: 150px;
+  width: 350px;
+  padding: 10px;
+  z-index: 2;
+}
+
+.login input[type=text]{
+  width: 250px;
+  height: 30px;
+  background: transparent;
+  border: 1px solid rgba(255,255,255,0.6);
+  border-radius: 2px;
+  color: #fff;
+  font-family: 'Exo', sans-serif;
+  font-size: 16px;
+  font-weight: 400;
+  padding: 4px;
+}
+
+.login input[type=password]{
+  width: 250px;
+  height: 30px;
+  background: transparent;
+  border: 1px solid rgba(255,255,255,0.6);
+  border-radius: 2px;
+  color: #fff;
+  font-family: 'Exo', sans-serif;
+  font-size: 16px;
+  font-weight: 400;
+  padding: 4px;
+  margin-top: 10px;
+}
+
+.login input[type=submit]{
+  width: 260px;
+  height: 35px;
+  background: #fff;
+  border: 1px solid #fff;
+  cursor: pointer;
+  border-radius: 2px;
+  color: #a18d6c;
+  font-family: 'Exo', sans-serif;
+  font-size: 16px;
+  font-weight: 400;
+  padding: 6px;
+  margin-top: 10px;
+}
+
+.login input[type=button]:hover{
+  opacity: 0.8;
+}
+
+.login input[type=submit]:active{
+  opacity: 0.6;
+}
+
+.login input[type=text]:focus{
+  outline: none;
+  border: 1px solid rgba(255,255,255,0.9);
+}
+
+.login input[type=password]:focus{
+  outline: none;
+  border: 1px solid rgba(255,255,255,0.9);
+}
+
+.login input[type=submit]:focus{
+  outline: none;
+}
+
+::-webkit-input-placeholder{
+   color: rgba(255,255,255,0.6);
+}
+
+::-moz-input-placeholder{
+   color: rgba(255,255,255,0.6);
+}
+    </style>
+
+    
+
+    
   </head>
+
   <body>
+
     <div class="body"></div>
-		<p><span>PANEL DE CONTROL, CLINICA DENTAL ENDOPERIO</span></p>
-		<div class="grad"></div>
-		<div class="header">
-			<div>Endoperio</div>
-		</div>
-		<br>
+    <p><span>Panel de control, clinica dental Endoperio</span></p>
+    <div class="grad"></div>
+    <div class="header">
+      <div>Endoperio</div>
+    </div>
+    <br>
         <form action="System/php/procesar_login.php" method="POST" class="login">
-				<input type="text" placeholder="user@host.com" name="nombre"><br>
-				<input type="password" placeholder="password" name="password"><br>
-				<input type="submit" value="Login">
-      	</form>
+        <input type="text" placeholder="tucorreo@algo.com" name="nombre"><br>
+        <input type="password" placeholder="password" name="password"><br>
+        <input type="submit" value="Login">
+        </form>
 
     <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
 
