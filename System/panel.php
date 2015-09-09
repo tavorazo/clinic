0368424408f68b580e1c2232bf7f2904d6866707
kurabe <arkangel.exe@gmail.com>
2015-09-09 17:34:23 -0500

    diebloas

--- System/panel.php
+++ System/panel.php
@@ -156,12 +156,8 @@ if($_SESSION['rol']=='admin' || $_SESSION['rol']=='almacen'){
       echo '<div style="background: #FFFFFF; width:96%; min-height: 90px; margin-bottom:3%; " id="txt2">
           <img src="images/alert.png" width="20px" alt="" style="float:left; margin-right:10px">
           <p>Inventario:</p> ';
-
-
-					
-
 					  $result2 = mysqli_query($conn,"SELECT * FROM inventario where reabastesible='1'");
-
+            echo "no manches";
 					  while ($row2 = mysqli_fetch_array($result2, MYSQL_NUM)) {
 
 						  if($row2[6]>$row2[4]){
