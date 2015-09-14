d1ee97ccd4b180c01c4fe7ba203ea030d55d5209
kurabe <arkangel.exe@gmail.com>
2015-09-14 02:16:21 -0500

    query panel correction

--- System/panel.php
+++ System/panel.php
@@ -86,7 +86,7 @@ if($_SESSION['rol']=='admin' || $_SESSION['rol']=='almacen'){
        echo "<br>Canditad de existencia en el inventario: ", $row2["cantidad"];
        echo "</div>
        <div id='botn3' style='margin-left:80%; width:100px; background: #FFFFFF; border:1px solid #848484;'>
-       <a href='inventario/inventario_editar.php?id=",$row2["id"],"'>Abastecer</a></div>";
+       <a href='inventario/inventario_editar.php?id=",$row2["id_producto"],"'>Abastecer</a></div>";
      }
   } 
 } 
