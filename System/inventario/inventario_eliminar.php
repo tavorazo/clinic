ffc8af63522d3f83ffd2cb41307839c2c7a27784
Octavio Razo <octavio.razo.verduzco@gmail.com>
2015-09-23 17:10:00 -0500

    recuperación almacén y contabilidad

--- System/inventario/inventario_eliminar.php
+++ System/inventario/inventario_eliminar.php
@@ -1,22 +1,11 @@
 <?php
 $id = $_GET['id'];
-
-/*$dbh = mysql_connect('localhost','root','') or die('Error de conexion: ' . mysql_error() );
-$base = mysql_select_db('Endoperio', $dbh) or die('Error de seleccion de base: ' . mysql_error() );
-/*$select = 'select * from Inventario where id_producto="'.$id.'";';
-$resul = mysql_query($select, $dbh) or die ("problema con la solicitud");
-$renglon = mysql_fetch_assoc($resul);
-$imagen = $renglon['imagen'];
-unlink($imagen);*/
-
 include('../php/base.php');
-include('../php/base3.php');
-	$eliminar = 'delete from inventario where id_producto="'.$id.'";';
-
-	if(!mysql_query($eliminar, $dbh))
-		die('Error de consulta: '.mysql_error());
-	mysql_close($conexion);
+$eliminar = 'DELETE from inventario where id_producto="'.$id.'";';
+//if(!mysql_query($eliminar, $dbh))
+if(!$conn->query($eliminar))
+	die('Error de consulta: '.mysqli_error());
+$conn->close();
+//mysql_close($conexion);
 header ("Location: ../almacen.php");
-
-
 ?>
--- System/php/procesar_instrumental.php
+++ System/php/procesar_instrumental.php
@@ -1,63 +1,55 @@
 <!DOCTYPE html>
-
 <html>
 <head>
-<meta http-equiv="Content-type" content="text/html;charset=UTF-8"/>
-  	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-
-  <title>Listo</title>
-	  <link rel="stylesheet" type="text/css" href="../css/texto.css"/>
-
+	<meta http-equiv="Content-type" content="text/html;charset=UTF-8"/>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Listo</title>
+	<link rel="stylesheet" type="text/css" href="../css/texto.css"/>
 	<style type="text/css" media="screen">
-
-	 body{
-	  background: steelblue;
-	  color: #1C1C1C;
-	 }
-	 a, a:hover{
-	  color: white;
-	  text-decoration: none;
-	 }
+		body{ background: steelblue; color: #1C1C1C; }
+		a, a:hover{ color: white; text-decoration: none; }
 	</style>
-
 </head>
 <body>
 	<?php
-		@session_start();
-		date_default_timezone_set("Mexico/General");
-		
-		include("base.php");
-		include("base3.php");
-		
-		$id_usuario = $_SESSION['u'];
-		$comprador = $_POST['usuario'];
-		$diplomado = $_POST['diplomado'];
-		$precio = $_POST['precio'];
-		$tipo_pago = $_POST['pago'];
-		$descripcion_pago = $_POST['descripcion'];
-		$s = date(W);
-		$y = date(Y);
+	@session_start();
+	date_default_timezone_set("Mexico/General");
+	
+	include("base.php");
+	//include("base3.php");
+	
+	$id_usuario = $_SESSION['u'];
+	$comprador = $_POST['usuario'];
+	$diplomado = $_POST['diplomado'];
+	$precio = $_POST['precio'];
+	$tipo_pago = $_POST['pago'];
+	$descripcion_pago = $_POST['descripcion'];
+	$s = date(W);
+	$y = date(Y);
+	$insertar_historial = "INSERT into historial_instrumental (id_usuario, id_comprador, diplomado_descripcion, precio, tipo_pago, descripcion_pago, fecha, semana, y) values ('$id_usuario', '$comprador', '$diplomado', '$precio', '$tipo_pago', '$descripcion_pago', now(),'$s','$y')";
+	if(!$conn->query($insertar_historial))
+		die('Error de consulta: '.mysqli_error($conn));
+	//if(!mysql_query($insertar2, $conexion))
+		//die('Error de consulta: '.mysql_error());
+	$insertar_recibo = "INSERT into recibos (cantidad, descripcion, total, fecha, comprador, vendedor) values ('1','$diplomado','$precio',now(),'$comprador','$id_usuario')";
+	if(!$conn->query($insertar_recibo))
+		die('Error de consulta: '.mysqli_error($conn));
+	$id_recibo = $conn->insert_id;
+	//if(!mysql_query($recibo, $conexion))
+		//die('Error de consulta: '.mysql_error());
+	//$id_recibo = mysql_insert_id();
 
-				$insertar2 = "insert into historial_instrumental (id_usuario, id_comprador, diplomado_descripcion, precio, tipo_pago, descripcion_pago, fecha, semana, y) values ('$id_usuario', '$comprador', '$diplomado', '$precio', '$tipo_pago', '$descripcion_pago', now(),'$s','$y')";
-
-				if(!mysql_query($insertar2, $conexion))
-					die('Error de consulta: '.mysql_error());
-
-				$recibo = "insert into recibos (cantidad, descripcion, total, fecha, comprador, vendedor) values ('1','$diplomado','$precio',now(),'$comprador','$id_usuario')";
-				if(!mysql_query($recibo, $conexion))
-					die('Error de consulta: '.mysql_error());
-				$id_recibo = mysql_insert_id();
-					//echo " realizada con éxito";
-					echo '<br><br><br><center><img src="../images/endoperio2.png" width="100px" alt=""> <br> ';
-					echo "Compra &eacute;xitosa<br><br><br>";
-					echo '<div style="  padding:9px; border:1px solid #E6E6E6; height:18px; width:120px; margin-top:12px; text-align:center; margin-right:10px ">';
-					echo "<a href='../panel.php'>Regresar </a></center></div>";
-					echo '<div style="  padding:9px; border:1px solid #E6E6E6; height:18px; width:120px; margin-top:12px; text-align:center; margin-right:10px ">';
-					echo '<a href="../php/imprimir_recibo.php?id_recibo=',$id_recibo,'" style="color:white">Imprimir Recibo</a></div>';
-			
+	echo '<br><br><br><center><img src="../images/endoperio2.png" width="100px" alt=""> <br> ';
+	echo "Compra &eacute;xitosa<br><br><br>";
+	echo '<div style="  padding:9px; border:1px solid #E6E6E6; height:18px; width:120px; margin-top:12px; text-align:center; margin-right:10px ">';
+	echo "<a href='lista_usuarios.php' style='color:#fff'>Regresar </a></center></div>";
+	echo '<center><div style="  padding:9px; border:1px solid #E6E6E6; height:18px; width:120px; margin-top:12px; text-align:center; margin-right:10px ">';
+	echo '<a href="../php/imprimir_recibo.php?id_recibo=',$id_recibo,'" style="color:white">Imprimir Recibo</a></div></center>';
+	
 				//echo'<META HTTP-EQUIV="Refresh" CONTENT="1; URL=../panel.php">';
+	//mysql_close($conexion);
+	$conn->close();
 
-				mysql_close($conexion);
 	?>
-	</body>
+</body>
 </html>
