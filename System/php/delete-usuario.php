a11ff6d393e8c7b525908ce534f7c5c3128db5ec
Octavio Razo <octavio.razo.verduzco@gmail.com>
2015-09-23 18:37:34 -0500

    modificación de php

--- System/php/delete-usuario.php
+++ System/php/delete-usuario.php
@@ -1,25 +1,24 @@
 <?php
 $id = $_GET['id'];
 
-/*
-$dbh = mysql_connect('localhost','root','') or die('Error de conexion: ' . mysql_error() );
-$base = mysql_select_db('Endoperio', $dbh) or die('Error de seleccion de base: ' . mysql_error() );*/
-
 include('base.php');
-include('base3.php');
-include('base2.php');
+//include('base3.php');
+//include('base2.php');
 
-$select = 'select * from usuarios where id_usuario="'.$id.'";';
-$resul = mysql_query($select, $dbh) or die ("problema con la solicitud");
-$renglon = mysql_fetch_assoc($resul);
+$select = 'SELECT * from usuarios where id_usuario="'.$id.'";';
+$resul = $conn->query($select);
+$renglon = $resul->fetch_assoc();
+//$resul = mysql_query($select, $dbh) or die ("problema con la solicitud");
+//$renglon = mysql_fetch_assoc($resul);
 
-	$eliminar = 'delete from usuarios where id_usuario="'.$id.'";';
+	$eliminar = 'DELETE from usuarios where id_usuario="'.$id.'";';
 
-	if(!mysql_query($eliminar, $dbh))
-		die('Error de consulta: '.mysql_error());
-	mysql_close($conexion);
+	//if(!mysql_query($eliminar, $dbh))
+	if(!$conn->query($eliminar))
+		die('Error de consulta: '.mysqli_error($conn));
+	//mysql_close($conexion);
+	$conn->close();
 
 header('location:lista_usuarios.php');
-
 
 ?>
