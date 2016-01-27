f26f50d4d01ef20017f77b4164484873bcd871f2
adrianVaNa <adrian.v.navarrete@gmail.com>
2016-01-26 22:03:55 -0600

    Plugin para cámara intra
    Se agregó el plugin para la cámara intra!

--- System/php/camara_intra.php
+++ System/php/camara_intra.php
@@ -10,76 +10,21 @@ $titulo ="Camara intra";
 include ("../+/head2.php");
 
 ?>
+ <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
+ <script src="//ajax.googleapis.com/ajax/libs/swfobject/2.2/swfobject.js" type="text/javascript"></script>
+ <script src="../scriptcam/scriptcam.js" type="text/javascript"></script>
+ <script src="../scriptcam/camara.js" type="text/javascript"></script> 
 
-    <h3>Camara intra. </h3>
+    <h3>Camara intra. </h3><br>
 	<div id="container" style="float:left; margin-left:15px; margin-right:15px;">
-        <video autoplay id="videoElement" >
-         
-        </video>
     </div>
+    <img id="foto" style="width:500px;height:375px;" style="float:right;" />
+	<p><img src="../scriptcam/webcamlogo.png" style="vertical-align:text-top"/>
+	<select id="cameraNames" size="1" onChange="changeCamera()" style="width:245px;font-size:10px;height:25px;">
+	</select></p>
 
-    <canvas id="canvas" width="500" height="375"  ></canvas>
+    <br><input type="button" value="Capturar" id="save" class="campoT" onclick="base64_tofield_and_image()">
+    <br><br> 
 
-    <input type="button" value="Capturar" id="save" class="buttom" />
-    <br><br>    
-    <label style="float:left; width:150px; margin-right:15%">Foto de ingreso</label>
-    <input type="file" name="imagen" class="campoT" style="float:left; witdh:200px"><br>
-
-   <script>
-        var video = document.querySelector("#videoElement");
-
-        navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia || navigator.oGetUserMedia;
-         
-        if (navigator.getUserMedia) {       
-            navigator.getUserMedia({video: true}, handleVideo, videoError);
-        }
-         
-        function handleVideo(stream) {
-            video.src = window.URL.createObjectURL(stream);
-        }
-         
-        function videoError(e) {
-        }
-        var v,canvas,context,w,h;
-        var sel = document.getElementById('fileselect'); // get reference to file select input element
-
-        document.addEventListener('DOMContentLoaded', function(){
-            v = document.getElementById('videoElement');
-            canvas = document.getElementById('canvas');
-            context = canvas.getContext('2d');
-            w = canvas.width;
-            h = canvas.height;
-
-        },false);
-
-        function draw(v,c,w,h) {
-            if(v.paused || v.ended) return false; // if no video, exit here
-            context.drawImage(v,0,0,w,h); // draw video feed to canvas      
-           var uri = canvas.toDataURL("image/png"); // convert canvas to data URI
-        }
-
-        document.getElementById('save').addEventListener('click',function(e){
-            
-            draw(v,context,w,h); // when save button is clicked, draw video feed to canvas
-            
-        });
-
-        var fr;
-
-        sel.addEventListener('change',function(e){
-            var f = sel.files[0]; // get selected file (camera capture)
-            
-            fr = new FileReader();
-            fr.onload = receivedData; // add onload event
-            fr.readAsDataURL(f); // get captured image as data URI
-        })
-
-        var button = document.getElementById('btn-download');
-		button.addEventListener('click', function (e) {
-    		var dataURL = canvas.toDataURL('image/png');
-    			button.href = dataURL;
-		
-        });
-    </script>
-
+  
 <?php  include ("../+/footer.php"); ?>
