784f62c35fe6ad871a95e9861767bfc76e8bd93c
adrianVaNa <adrian.v.navarrete@gmail.com>
2016-02-05 22:40:59 -0600

    Restauración de BD
    Olvidé restaurar la BD original

--- System/php/base.php
+++ System/php/base.php
@@ -1,9 +1,9 @@
 <?php
 error_reporting(E_ALL & ~E_NOTICE);
-$server = "localhost";
+$server = "endoperio.com.mx";
 $port = "3306";
-$username = "root";
-$password = "admin";
+$username = "endoper2_git";
+$password = "T~J{QM=4F;Q#";
 $db = "endoper2_1";
  try{
   //$conn = new PDO("mysql:host=$server;dbname= $db;charset=UTF8", $username, $password); 
