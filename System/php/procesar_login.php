55732ae53b4afc328382c69dea78a80c839ede19
adrianVaNa <adrian.v.navarrete@gmail.com>
2016-06-24 17:57:08 -0500

    Solución de bug que regresaba a index

--- System/php/procesar_login.php
+++ System/php/procesar_login.php
@@ -1,124 +1,49 @@
+<?php
+    /* Empezamos la sesión */
+    session_start();
+
+    include('base.php');
+    $sql      =   'SELECT * from usuarios where correo="'.$_POST['nombre'].'" AND password="'.$_POST['password'].'";';
+    $resul    =   $conn->query($sql);
+    $renglon  = $resul->fetch_assoc();
+
+    if($renglon['correo']!=''){
+        $_SESSION['u']        = $renglon['id_usuario'];
+        $_SESSION['rol']      = $renglon['rol'];
+        $_SESSION['nombres']  = $renglon['nombres'];
+        $_SESSION['sucursal'] = $renglon['id_sucursal'];
+
+        $msj_r = "<br><h2>Iniciando sesi&oacute;n</h2><br><h2> Bienvenido ".utf8_encode($renglon['nombres'])."</h2>";
+
+        if ($renglon['rol'] == 'merca') 
+          $url_r = '1; URL=../merca/';
+        else
+          $url_r = '1; URL=../panel.php"';
+    }
+    else{
+        $msj_r = '<br><h2>Tu inicio de sesi&oacute;n ha sido incorrecto <br> vuelve a intentar</h2>';
+        $url_r = '3; URL=../../log.php';
+    }
+    
+?>
+ 
 <!DOCTYPE html>
-<html class="html">
- <head>
-
-  <meta http-equiv="Content-type" content="text/html;charset=UTF-8"/>
-  <meta name="generator" content="7.0.314.244"/>
-
-  <title>Endoperio | <?php echo $titulo; ?></title>
-  <link rel="shortcut icon" type="x-icon" href="../images/icon.png" /><!--para logo en barra-->
-  <!-- CSS -->
-  <link rel="stylesheet" type="text/css" href="../css/s1.css"/>
-  <link rel="stylesheet" type="text/css" href="../css/texto.css"/>
-  
-  <link rel="stylesheet" type="text/css" href="../css/site_global.css?417434784"/>
-  <link rel="stylesheet" type="text/css" href="../css/master_modif-master.css?3789319557"/>
-  <link rel="stylesheet" type="text/css" href="../css/agregar_receta.css?4008918636" id="pagesheet"/>
-  <!-- Other scripts -->
-  <script type="text/javascript">
-   document.documentElement.className += ' js';
-</script>
-   </head>
- <body>
-
-  <div class="clearfix" id="page"><!-- column -->
-   <div class="position_content" id="page_position_content">
-    <div class="clearfix colelem" id="pu1145"><!-- group -->
-     <div class="browser_width grpelem" id="u1145"><!-- group -->
-      <div class="clearfix" id="u1145_align_to_page">
-       <a class="nonblock nontext clip_frame grpelem" id="u1147" href="../index.php"><!-- image --><img class="block" id="u1147_img" src="../images/logo-endoperio-dental-center.jpg" alt="" width="134" height="38"/></a>
-       <div class="grpelem" id="u1149"><!-- simple frame --></div>
-       <div class="clearfix grpelem" id="u1150-5"><!-- content -->
-         <p></p>
-       </div>
-      </div>
-     </div>
-    </div>
-    <div class="clearfix colelem" id="pu1132-5"><!-- group -->
-     <div class="clearfix grpelem" id="u1132-5"><!-- content -->
-     </div>
-     <div class="clearfix grpelem" id="u1134-4">
-
-     <a   href="<?php echo $enlace; ?>" style="float:left; margin-right:10px"> << Regresar </a> <h3  style="margin-right:5px">|</h3>
-	<?php
-	@session_start();
-	$a=$_POST['nombre'];
-	$b=$_POST['password'];
-
-	require_once("base.php");
-
-	$select = 'SELECT * from usuarios where correo="'.$a.'" and password="'.$b.'";';
-
-	//$resul = mysqli_query($conn, $select);
-	$resul = $conn->query($select);
-	//$renglon = mysqli_fetch_assoc($resul);
-	$renglon = $resul->fetch_assoc();
-
-	echo "Verificando. . .";
-  	?>
-  	<center><br><br><br>
-	<img src="../images/35.gif" alt="">
-	<?php
-	if($renglon['correo']!=''){
-		
-		$_SESSION['u']=$renglon['id_usuario'];
-		//echo $renglon['id_usuario'];
-		$_SESSION['rol']=$renglon['rol'];
-		$_SESSION['nombres'] = $renglon['nombres'];
-    $_SESSION['sucursal'] = $renglon['id_sucursal'];
-		echo "<br>Correcto. . . Bienvenido ".$_SESSION['nombres'];
-		if ($renglon['rol'] == 'merca') 
-			echo '<META HTTP-EQUIV="Refresh" CONTENT="5; URL=../merca/">';
-		else
-			echo '<META HTTP-EQUIV="Refresh" CONTENT="5; URL=../panel.php">';
-	}
-	else{
-		echo '<br><h2>Tu inicio de sesi&oacute;n ha sido incorrecto <br> vuelve a intentar  <META HTTP-EQUIV="Refresh" CONTENT="5; URL=../../log.php">';
-	}
-	?>
-	
-	</body>
-</html>
-
-
-    </div>
-    <div class="verticalspacer"></div>
-  </div>
-</div>
-<div class="preload_images">
- <img class="preload" src="../images/u372-r.png" alt=""/>
- <img class="preload" src="../images/u376-r.png" alt=""/>
- <img class="preload" src="../images/u377_states-r.png" alt=""/>
- <img class="preload" src="../images/u377_states-a.png" alt=""/>
- <img class="preload" src="../images/u378_states-r.png" alt=""/>
- <img class="preload" src="../images/u378_states-a.png" alt=""/>
- <img class="preload" src="../images/u480_states-r.png" alt=""/>
- <img class="preload" src="../images/u480_states-a.png" alt=""/>
- <img class="preload" src="../images/u550_states-r.png" alt=""/>
- <img class="preload" src="../images/u550_states-a.png" alt=""/>
- <img class="preload" src="../images/u552_states-r.png" alt=""/>
- <img class="preload" src="../images/u552_states-a.png" alt=""/>
-</div>
-<!-- JS includes -->
-<script type="text/javascript">
-if (document.location.protocol != 'https:') document.write('\x3Cscript src="http://musecdn.businesscatalyst.com/scripts/4.0/jquery-1.8.3.min.js" type="text/javascript">\x3C/script>');
-</script>
-<script type="text/javascript">
-window.jQuery || document.write('\x3Cscript src="../scripts/jquery-1.8.3.min.js" type="text/javascript">\x3C/script>');
-</script>
-<script src="../scripts/museutils.js?3840766194" type="text/javascript"></script>
-<script src="../scripts/jquery.tobrowserwidth.js?3842421675" type="text/javascript"></script>
-<script src="../scripts/jquery.watch.js?4068933136" type="text/javascript"></script>
-<!-- Other scripts -->
-<script type="text/javascript">
-$(document).ready(function() { try {
-  Muse.Utils.transformMarkupToFixBrowserProblemsPreInit();/* body */
-  $('.browser_width').toBrowserWidth();/* browser width elements */
-  Muse.Utils.prepHyperlinks(true);/* body */
-  Muse.Utils.fullPage('#page');/* 100% height page */
-  Muse.Utils.showWidgetsWhenReady();/* body */
-  Muse.Utils.transformMarkupToFixBrowserProblems();/* body */
-} catch(e) { Muse.Assert.fail('Error calling selector function:' + e); }});
-</script>
-   </body>
+<html lang="es">
+    <head>
+            <title>Endoperio | Iniciando sesión</title>
+    </head>
+ 
+    <body>
+        <div>
+            <section>
+              <center>
+                <img src="../images/35.gif" alt="">
+                <p style="font-family: Arial, Helvetica, sans-serif"> 
+                    <?php echo $msj_r; ?>
+                    <META HTTP-EQUIV="Refresh" CONTENT="<?php echo $url_r?>">
+                </p>
+              </center>
+            </section>
+        </div>
+    </body>
 </html>
